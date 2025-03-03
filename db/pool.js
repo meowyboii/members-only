d1a5fb9dce552b9429b2bfaa5869edab04807b3f
Tim <mideltim@gmail.com>
2025-03-03 17:28:25 +0800

    chore: change pool initialization to connection string

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const HOST = process.env.PGHOST;
-const USER = process.env.PGUSER;
-const DATABASE = process.env.PGDATABASE;
-const PASSWORD = process.env.PGPASSWORD;
-const DB_PORT = process.env.PGPORT;
+const CONNECTION_STRING = process.env.DATABASE_URL;
 
 module.exports = new Pool({
-  host: HOST,
-  user: USER,
-  database: DATABASE,
-  password: PASSWORD,
-  port: DB_PORT,
+  connectionString: CONNECTION_STRING,
 });
